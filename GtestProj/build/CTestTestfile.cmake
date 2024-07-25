a875d150a960778c77f0de79b3e5b945e9dae500
Michin <niesmialymichal@gmail.com>
2024-07-25 22:57:43 +0200

    delete build

--- GtestProj/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/SE/Projects/Concepts/GtestProj
-# Build directory: C:/SE/Projects/Concepts/GtestProj/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/SE/Projects/Concepts/GtestProj/build/hello_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- GtestProj/build/hello_test[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[HelloTest.BasicAssertions]=]  C:/SE/Projects/Concepts/GtestProj/build/hello_test.exe [==[--gtest_filter=HelloTest.BasicAssertions]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[HelloTest.BasicAssertions]=]  PROPERTIES WORKING_DIRECTORY C:/SE/Projects/Concepts/GtestProj/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  hello_test_TESTS HelloTest.BasicAssertions)
