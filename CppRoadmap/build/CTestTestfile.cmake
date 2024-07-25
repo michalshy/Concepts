d433875505bed98b69151b0e1d13138583040cde
Michin <niesmialymichal@gmail.com>
2024-07-25 21:52:25 +0200

    fix

--- CppRoadmap/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/SE/Projects/Concepts/CppRoadmap
-# Build directory: C:/SE/Projects/Concepts/CppRoadmap/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
