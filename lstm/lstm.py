e284676e3b1831c505a7197ce72cc8a3eb9e096b
michalshy <91937056+michalshy@users.noreply.github.com>
2024-10-29 21:30:09 +0100

    up

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 def create_dataset(dataset):
     data = []
     temp = []
-    for j in range(3):
+    for j in range(5):
         a = dataset[len(dataset) - j - 1]
         temp.append(a)
     data.append(temp)
@@ -25,12 +25,13 @@ _path = []
 finData =[]
 model: keras.Model = keras.models.load_model(r'jakub.keras')
 df = pd.read_csv(r'agv.pkl', low_memory=False)
-df = df[['X-coordinate', 'Y-coordinate', 'Heading']]
+df = df[['X-coordinate', 'Y-coordinate', 'Heading','Current segment']]
 df['X-coordinate'] = pd.to_numeric(df['X-coordinate'], errors='coerce')
 df['Y-coordinate'] = pd.to_numeric(df['Y-coordinate'], errors='coerce')
 df['Heading'] = pd.to_numeric(df['Heading'], errors='coerce')
 df = df.dropna()
-#df = df[df['Current segment'] == 59.0]
+df = df[df['Current segment'] == 59.0]
+df = df[['X-coordinate', 'Y-coordinate', 'Heading']]
 _scaler = MinMaxScaler()
 df_scaled = df.copy()
 df_scaled[['X-coordinate', 'Y-coordinate', 'Heading']] = _scaler.fit_transform(df[['X-coordinate', 'Y-coordinate', 'Heading']])
@@ -45,18 +46,18 @@ for i in range(len(to_drive)):
 # plt.scatter(dataNP[:, 0], dataNP[:, 1])
 # plt.show()
 
-for i in range(40):
-    finData.append(to_drive[130 + i])
+for i in range(39):
+    finData.append(to_drive[131 + i])
 finNp = np.array(finData)
 plt.plot(finNp[:, 0], finNp[:, 1])
 plt.show()
 
 finData.clear()
 
-for i in range(3):
-    finData.append(to_drive[130 + i])
+for i in range(5):
+    finData.append(to_drive[131 + i])
 
-for i in range(36):
+for i in range(30):
     df2 = pd.DataFrame(finData, columns=['X-coordinate', 'Y-coordinate', 'Heading'])
     df2 = df2.values
     df2 = df2.astype('float32')
